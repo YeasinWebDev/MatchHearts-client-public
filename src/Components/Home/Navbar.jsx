64f9331a29ef2f93da6d164ecb254b3972a0ca2d
Yeasin <ar9485727@gmail.com>
2024-06-01 11:58:05 +0600

    navbar logo added

--- src/Components/Home/Navbar.jsx
+++ src/Components/Home/Navbar.jsx
@@ -11,43 +11,46 @@ function Navbar() {
     };
 
     return (
-        <div className='w-full border-2 border-red-800 h-full'>
-            <div className={`flex justify-between items-center border-2 w-full md:px-20 px-5 py-2 ${dark ? 'bg-[#121212]' : 'bg-[#FFFFFF]'}`}>
+        <>
+
+            <div className={`flex justify-between items-center border-2 w-full md:px-20 px-5 py-2 rounded-xl ${dark ? 'bg-[#232121]' : 'bg-[#FFFFFF]'}`}>
 
                 {/* left */}
-                <div className='w-10 h-10 bg-red-800'></div>
+                <div className='w-16 h-16 rounded-xl'>
+                    <img className='w-full h-full rounded-xl' src="https://i.ibb.co/0JfPLPG/main.png" alt="" />
+                </div>
 
                 {/* right */}
-                <div className={`lg:hidden ${dark? 'text-white' :"text-black"}`} onClick={toggleMenu}>
+                <div className={`lg:hidden ${dark ? 'text-white' : "text-black"}`} onClick={toggleMenu}>
                     <button className="text-2xl">&#9776;</button>
                 </div>
 
                 {/* Menu for large screens */}
-                <div className={`hidden lg:flex ${dark ? 'bg-[#121212] text-white' : 'bg-[#FFFFFF] text-black'}`}>
+                <div className={`hidden lg:flex ${dark ? 'bg-[#232121] text-white' : 'bg-[#FFFFFF] text-black'}`}>
                     <ul className='flex justify-center gap-5'>
-                        <NavLink to={'/'} className={({ isActive }) => `font-semibold text-lg border-b-2 cursor-pointer ${isActive ? 'bg-orange-500 rounded-xl p-2' : 'p-2'}`}><li className=''>Home</li></NavLink>
-                        <NavLink to={'#'} className={({ isActive }) => `font-semibold text-lg border-b-2 cursor-pointer ${isActive ? 'bg-orange-500 rounded-xl p-2' : 'p-2'}`}><li className=''>Biodatas</li></NavLink>
-                        <NavLink to={'#'} className={({ isActive }) => `font-semibold text-lg border-b-2 cursor-pointer ${isActive ? 'bg-orange-500 rounded-xl p-2' : 'p-2'}`}><li className=''>About Us</li></NavLink>
-                        <NavLink to={'#'} className={({ isActive }) => `font-semibold text-lg border-b-2 cursor-pointer ${isActive ? 'bg-orange-500 rounded-xl p-2' : 'p-2'}`}><li className=''>Contact Us</li></NavLink>
-                        <NavLink to={'#'} className={({ isActive }) => `font-semibold text-lg border-b-2 cursor-pointer ${isActive ? 'bg-orange-500 rounded-xl p-2' : 'p-2'}`}><li className=''>Login</li></NavLink>
-                        <NavLink to={'#'} className={({ isActive }) => `font-semibold text-lg border-b-2 cursor-pointer ${isActive ? 'bg-orange-500 rounded-xl p-2' : 'p-2'}`}><li className=''>Dashboard</li></NavLink>
+                        <NavLink to={'/'} className={({ isActive }) => `font-semibold text-lg border-b-2 cursor-pointer ${isActive ? 'bg-orange-500 rounded-xl p-2' : 'p-2'}`}><li>Home</li></NavLink>
+                        <NavLink to={'#'} className={({ isActive }) => `font-semibold text-lg border-b-2 cursor-pointer ${isActive ? 'bg-orange-500 rounded-xl p-2' : 'p-2'}`}><li>Biodatas</li></NavLink>
+                        <NavLink to={'#'} className={({ isActive }) => `font-semibold text-lg border-b-2 cursor-pointer ${isActive ? 'bg-orange-500 rounded-xl p-2' : 'p-2'}`}><li>About Us</li></NavLink>
+                        <NavLink to={'#'} className={({ isActive }) => `font-semibold text-lg border-b-2 cursor-pointer ${isActive ? 'bg-orange-500 rounded-xl p-2' : 'p-2'}`}><li>Contact Us</li></NavLink>
+                        <NavLink to={'#'} className={({ isActive }) => `font-semibold text-lg border-b-2 cursor-pointer ${isActive ? 'bg-orange-500 rounded-xl p-2' : 'p-2'}`}><li>Login</li></NavLink>
+                        <NavLink to={'#'} className={({ isActive }) => `font-semibold text-lg border-b-2 cursor-pointer ${isActive ? 'bg-orange-500 rounded-xl p-2' : 'p-2'}`}><li>Dashboard</li></NavLink>
                     </ul>
                 </div>
 
             </div>
 
             {/* Menu for small screens */}
-            <div className={`${isOpen ? 'block transition-all' : 'hidden transition-all'} absolute transform transition-transform duration-300 ease-in-out px-8 py-5  rounded-xl lg:hidden ${dark ? 'bg-[#121212] text-white' : 'bg-[#FFFFFF] text-black'} border-2`}>
+            <div className={`${isOpen ? 'block transition-all' : 'hidden transition-all'} absolute transform transition-transform duration-300 ease-in-out px-8 py-5  rounded-xl lg:hidden ${dark ? 'bg-[#232121] text-white' : 'bg-[#FFFFFF] text-black'} border-2`}>
                 <ul className='flex flex-col  gap-2 py-2'>
-                    <NavLink to={'/'} className={({ isActive }) => `font-semibold text-lg border-b-2 cursor-pointer ${isActive ? 'bg-orange-500 rounded-xl p-2' : 'p-2'}`}><li className=''>Home</li></NavLink>
-                    <NavLink to={'#'} className={({ isActive }) => `font-semibold text-lg border-b-2 cursor-pointer ${isActive ? 'bg-orange-500 rounded-xl p-2' : 'p-2'}`}><li className=''>Biodatas</li></NavLink>
-                    <NavLink to={'#'} className={({ isActive }) => `font-semibold text-lg border-b-2 cursor-pointer ${isActive ? 'bg-orange-500 rounded-xl p-2' : 'p-2'}`}><li className=''>About Us</li></NavLink>
-                    <NavLink to={'#'} className={({ isActive }) => `font-semibold text-lg border-b-2 cursor-pointer ${isActive ? 'bg-orange-500 rounded-xl p-2' : 'p-2'}`}><li className=''>Contact Us</li></NavLink>
-                    <NavLink to={'#'} className={({ isActive }) => `font-semibold text-lg border-b-2 cursor-pointer ${isActive ? 'bg-orange-500 rounded-xl p-2' : 'p-2'}`}><li className=''>Login</li></NavLink>
-                    <NavLink to={'#'} className={({ isActive }) => `font-semibold text-lg border-b-2 cursor-pointer ${isActive ? 'bg-orange-500 rounded-xl p-2' : 'p-2'}`}><li className=''>Dashboard</li></NavLink>
+                    <NavLink to={'/'} className={({ isActive }) => `font-semibold text-lg border-b-2 cursor-pointer ${isActive ? 'bg-orange-500 rounded-xl p-2' : 'p-2'}`}><li>Home</li></NavLink>
+                    <NavLink to={'#'} className={({ isActive }) => `font-semibold text-lg border-b-2 cursor-pointer ${isActive ? 'bg-orange-500 rounded-xl p-2' : 'p-2'}`}><li>Biodatas</li></NavLink>
+                    <NavLink to={'#'} className={({ isActive }) => `font-semibold text-lg border-b-2 cursor-pointer ${isActive ? 'bg-orange-500 rounded-xl p-2' : 'p-2'}`}><li>About Us</li></NavLink>
+                    <NavLink to={'#'} className={({ isActive }) => `font-semibold text-lg border-b-2 cursor-pointer ${isActive ? 'bg-orange-500 rounded-xl p-2' : 'p-2'}`}><li>Contact Us</li></NavLink>
+                    <NavLink to={'#'} className={({ isActive }) => `font-semibold text-lg border-b-2 cursor-pointer ${isActive ? 'bg-orange-500 rounded-xl p-2' : 'p-2'}`}><li>Login</li></NavLink>
+                    <NavLink to={'#'} className={({ isActive }) => `font-semibold text-lg border-b-2 cursor-pointer ${isActive ? 'bg-orange-500 rounded-xl p-2' : 'p-2'}`}><li>Dashboard</li></NavLink>
                 </ul>
             </div>
-        </div>
+        </>
     )
 }
 
--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Hero from '../Components/Home/Hero'
 
 function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <Hero/>
     </div>
   )
 }
